0fd3aec71ad1216f249ff2abe49c36896f9c306e
Uriel Salischiker <uriel@urielsalis.me>
2023-02-11 16:43:32 +0100

    More sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ echo Connect to tunnel
 az config set extension.use_dynamic_install=yes_without_prompt
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
-sleep 5
+sleep 60
 
 echo Start Run Command
 
